a8c9906d87bd6467fec301eaa1f87711aa92ec9d
Darius <dariusepure1@gmail.com>
2026-08-02 16:00:40 +0300

    New colours

--- app/src/main/java/com/dariusepure/caractivitylog/di/AppModule.kt
+++ app/src/main/java/com/dariusepure/caractivitylog/di/AppModule.kt
@@ -28,9 +28,9 @@ object AppModule {
     @Singleton
     fun provideHttpClient(): HttpClient = HttpClient(OkHttp) {
         install(HttpTimeout) {
-            connectTimeoutMillis = 15000
-            socketTimeoutMillis = 15000
-            requestTimeoutMillis = 30000
+            connectTimeoutMillis = 20000
+            socketTimeoutMillis = 60000
+            requestTimeoutMillis = 90000
         }
         install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) {
             json(kotlinx.serialization.json.Json {
--- app/src/main/java/com/dariusepure/caractivitylog/ui/cars/BrandHelper.kt
+++ app/src/main/java/com/dariusepure/caractivitylog/ui/cars/BrandHelper.kt
@@ -18,20 +18,20 @@ object BrandHelper {
     fun getBrandColor(make: String): Long {
         return when (make.uppercase().trim()) {
             "ABARTH" -> 0xFFE30613
-            "ACURA" -> 0xFF111111
+            "ACURA" -> 0xFFCE1126
             "ALFA ROMEO" -> 0xFFB10B1B
             "ALPINE" -> 0xFF005BA3
             "ASTON MARTIN" -> 0xFF006B3D
-            "AUDI" -> 0xFFBBBBBB
+            "AUDI" -> 0xFFE30613
             "BENTLEY" -> 0xFF004225
             "BMW" -> 0xFF1C69D4
             "BUGATTI" -> 0xFF002244
             "BUICK" -> 0xFF0038A8
             "BYD" -> 0xFF00A3E0
-            "CADILLAC" -> 0xFF111111
+            "CADILLAC" -> 0xFFFFD700
             "CATERHAM" -> 0xFF004225
             "CHEVROLET" -> 0xFFEAAA00
-            "CHRYSLER" -> 0xFF111111
+            "CHRYSLER" -> 0xFF003399
             "CITROËN" -> 0xFFFF2D00
             "CUPRA" -> 0xFFD19F73
             "DACIA" -> 0xFF4E5B31
@@ -42,43 +42,43 @@ object BrandHelper {
             "FERRARI" -> 0xFFFF2800
             "FIAT" -> 0xFFCD1A1A
             "FORD" -> 0xFF003399
-            "GENESIS" -> 0xFF222222
+            "GENESIS" -> 0xFFB87333
             "GMC" -> 0xFFE31837
             "HONDA" -> 0xFFFF0000
             "HUMMER" -> 0xFF795548
             "HYUNDAI" -> 0xFF002C5F
-            "INFINITI" -> 0xFF222222
+            "INFINITI" -> 0xFF002C5F
             "ISUZU" -> 0xFFED1C24
             "JAGUAR" -> 0xFF006B3D
             "JEEP" -> 0xFF5B673A
             "KIA" -> 0xFFBB162B
-            "KOENIGSEGG" -> 0xFF111111
+            "KOENIGSEGG" -> 0xFF00A3E0
             "LAMBORGHINI" -> 0xFFFFCC00
             "LANCIA" -> 0xFF003399
             "LAND ROVER" -> 0xFF004225
-            "LEXUS" -> 0xFF111111
-            "LINCOLN" -> 0xFF222222
+            "LEXUS" -> 0xFF003399
+            "LINCOLN" -> 0xFF001E32
             "LOTUS" -> 0xFF004225
-            "LUCID" -> 0xFF111111
+            "LUCID" -> 0xFFD19F73
             "MASERATI" -> 0xFF003399
             "MAYBACH" -> 0xFF4B3621
             "MAZDA" -> 0xFF9E0B0E
             "MCLAREN" -> 0xFFFF8000
-            "MERCEDES-BENZ" -> 0xFFC0C0C0
+            "MERCEDES-BENZ" -> 0xFF00ADEF
             "MG" -> 0xFFBA0C2F
-            "MINI" -> 0xFF222222
+            "MINI" -> 0xFFF58025
             "MITSUBISHI" -> 0xFFEE1111
             "MORGAN" -> 0xFF004225
             "NISSAN" -> 0xFFC11B17
             "OPEL" -> 0xFFFFD700
-            "PAGANI" -> 0xFF111111
+            "PAGANI" -> 0xFF002244
             "PEUGEOT" -> 0xFF001E32
-            "POLESTAR" -> 0xFFBBBBBB
-            "PORSCHE" -> 0xFF111111
+            "POLESTAR" -> 0xFFE1B924
+            "PORSCHE" -> 0xFFD5001C
             "RAM" -> 0xFFE31837
             "RENAULT" -> 0xFFFFCC00
             "RIMAC" -> 0xFF00A3E0
-            "ROLLS-ROYCE" -> 0xFF111111
+            "ROLLS-ROYCE" -> 0xFF602F6B
             "ROVER" -> 0xFFB10B1B
             "SAAB" -> 0xFF004A99
             "SEAT" -> 0xFFE31837
@@ -89,7 +89,7 @@ object BrandHelper {
             "SUZUKI" -> 0xFFEE1111
             "TESLA" -> 0xFFCC0000
             "TOYOTA" -> 0xFFEB0A1E
-            "TRIUMPH" -> 0xFF111111
+            "TRIUMPH" -> 0xFF004225
             "TVR" -> 0xFF6A0DAD
             "VAUXHALL" -> 0xFFE31837
             "VOLKSWAGEN" -> 0xFF001E50
